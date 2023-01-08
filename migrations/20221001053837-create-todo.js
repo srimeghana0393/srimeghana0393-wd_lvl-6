beaf08dff92fc2f0772f916b316042ef659d6133
srimeghana0393 <srimeghana0393@gmail.com>
2023-01-08 13:14:58 +0530

    lvl9

--- migrations/20221001053837-create-todo.js
+++ migrations/20221001053837-create-todo.js
@@ -2,38 +2,33 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    //table for todo items
+    //create a table for todos in database
     await queryInterface.createTable("Todos", {
-      //id of todo
+      //id
       id: {
-        allowNull: false, // do not allow empty
-        autoIncrement: true,
-        primaryKey: true, //acts as a primary key
+        allowNull: false, //do not allow empty
+        autoIncrement: true, 
+        primaryKey: true, //id acts as primary key
         type: Sequelize.INTEGER, //type - number
       },
-
-      //title of the todo item
+      //title
       title: {
         type: Sequelize.STRING, //type - string
       },
-
-      //due date for the todo item
+      // duedate for an item 
       dueDate: {
         type: Sequelize.DATEONLY, //type - only date
       },
-
-      //status of the todo item
+      //completed status of an item
       completed: {
         type: Sequelize.BOOLEAN, //type - true or false
       },
-
-      //todo item created on
+      //when the item is created
       createdAt: {
         allowNull: false, //do not allow empty
         type: Sequelize.DATE, //type - date
       },
-
-      //todo item update on 
+      //when the item is updated
       updatedAt: {
         allowNull: false, //do not allow empty
         type: Sequelize.DATE, //type - date
